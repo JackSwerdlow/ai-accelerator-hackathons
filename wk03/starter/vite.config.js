26e74c7cf3b6383d4e5ef19459379f7f6f9a008b
Jack Swerdlow <jack.swerdlow@businessandtrade.gov.uk>
2026-06-03 15:09:17 +0100

    change hmr clientPort to PORT var in vite.config.js

--- wk03/starter/vite.config.js
+++ wk03/starter/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: PORT,
     strictPort: true,
-    hmr: { protocol: 'wss', host: HOSTNAME, clientPort: 5002 },
+    hmr: { protocol: 'wss', host: HOSTNAME, clientPort: PORT },
   },
   test: {
     globals: true,
